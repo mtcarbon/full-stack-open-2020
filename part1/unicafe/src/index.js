b6000f731032d2bfb1acd7cef68421adacae5537
brucite <mcarbone43@gmail.com>
2021-02-04 12:11:45 -0600

    Completed exercise 1.7 (unicafe step2), added total number of collected feedback, avg score, and percentage of positive feedback

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -33,10 +33,40 @@ const App = () => {
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
+    const [all, setAll] = useState(0)
+    const [avg, setAvg] = useState(0)
+    const [prcntPstv, setPrcnt] = useState(0)
     
-    const incGood = () => setGood(good + 1)
-    const incNeutral = () => setNeutral(neutral + 1)
-    const incBad = () => setBad(bad + 1)
+    const incGood = () => {
+        setGood(good + 1)
+        dsplyAll()
+        dsplyAvg(1)
+        dsplyPrcntPstv(1)
+    }
+
+    const incNeutral = () => {
+        setNeutral(neutral + 1)
+        dsplyAll()
+        dsplyAvg(0)
+        dsplyPrcntPstv(0)
+    }
+
+    const incBad = () => {
+        setBad(bad + 1)
+        dsplyAll()
+        dsplyAvg(-1)
+        dsplyPrcntPstv(0)
+    }
+    const dsplyAll = () =>  setAll(good + neutral + bad + 1)
+     
+    const dsplyAvg = (val) => {
+        val = val + good * 1 + bad * -1
+        setAvg(val/(all + 1))
+    } 
+
+    const dsplyPrcntPstv = (val) => {
+        setPrcnt( String( (good + val) / (all + 1) ) + ' %' )
+    } 
 
     return (
         <div>
@@ -48,6 +78,9 @@ const App = () => {
             <Line label='good' value={good}/>
             <Line label='neutral' value={neutral}/>
             <Line label='bad' value={bad}/>
+            <Line label='all' value={all}/>
+            <Line label='average' value={avg}/>
+            <Line label='positve' value={prcntPstv}/>
         </div>
     )
 }
